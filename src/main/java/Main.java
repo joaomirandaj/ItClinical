fbf3074b7c58b07e1ed86c674731078dba3b1d66
Joao Miranda <joaomirandaj@hotmail.com>
2022-05-06 04:17:16 +0100

    Exercise Complete.

--- src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        ReturnNthCharacter returnNthCharacter = new ReturnNthCharacter();
-
-        System.out.println(returnNthCharacter.returnNthCharacterFrom("ItCLINiCAL",1));
-
-    }
-}
